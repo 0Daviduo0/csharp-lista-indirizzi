a24e250456c8d667666b90ea68af622de1556b4e
Daviduo <davide0400@gmail.com>
2023-04-14 16:13:11 +0200

    aggiunta di commenti

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -12,6 +12,7 @@ namespace csharp_lista_indirizzi
             {
                 List<Indirizzo> indirizzi = new List<Indirizzo>();
 
+                //Utilizzo di using per fare in modo che capisca in autonomia quando chiudere il file
                 using (StreamReader inputstream = File.OpenText("addresses.csv"))
                 {
                     // Leggi la prima riga che contiene i nomi dei campi
@@ -25,14 +26,14 @@ namespace csharp_lista_indirizzi
                         // Dividi la riga in un array di valori
                         string[] valori = riga.Split(',');
 
-                        // Verifica se ci sono informazioni mancanti sull'indirizzo
+                        // Verifica se ci sono informazioni mancanti sull'indirizzo e avverte l'utente all'inizio dell'esecuzione
                         if (valori.Length < 6)
                         {
                             Console.WriteLine("Attenzione: controllare l'output per vedere quali campi sono mancanti.");
                             continue;
                         }
 
-                        // Verifica se ci sono valori mancanti e sostituiscili con una stringa predefinita
+                        // Verifica se ci sono valori mancanti e in caso vengono sostituiti con una stringa predefinita
                         string nome = string.IsNullOrWhiteSpace(valori[0]) ? "[Nome-mancante]" : valori[0];
                         string cognome = string.IsNullOrWhiteSpace(valori[1]) ? "[Cognome-mancante]" : valori[1];
                         string strada = string.IsNullOrWhiteSpace(valori[2]) ? "[Indirizzo-mancante]" : valori[2];
@@ -40,7 +41,7 @@ namespace csharp_lista_indirizzi
                         string provincia = string.IsNullOrWhiteSpace(valori[4]) ? "[Provincia-mancante]" : valori[4];
                         string cap = string.IsNullOrWhiteSpace(valori[5]) ? "[CAP-mancante]" : valori[5];
 
-                        // Crea l'oggetto Indirizzo e aggiungilo alla lista
+                        // Creazione lista con tutte le varie parti
                         Indirizzo indirizzo = new Indirizzo(nome, cognome, strada, città, provincia, cap);
                         indirizzi.Add(indirizzo);
                     }
@@ -58,6 +59,7 @@ namespace csharp_lista_indirizzi
                         $"CAP: {indirizzo.ZIP}");
                 }
             }
+            //messaggio di errore in caso ci sia un problema con il try
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
